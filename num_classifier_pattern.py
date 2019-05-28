2efd97c8407dfdc4d1de5d52f6a757a91a8d2121
Anirudh Das Bezzam <abezzam@stevens.edu>
2019-05-28 12:00:08 -0400

    Updatede predict

--- num_classifier_pattern.py
+++ num_classifier_pattern.py
@@ -15,16 +15,16 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 
-model.compile(optimizer='adam', loss='sparse_categorical_entropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=3)
 
 val_loss, val_acc = model.evaluate(x_test, y_test)
-model.save('number_reader')
+model.save('number_reader.model')
 new_model = tf.keras.models.load_model('number_reader.model')
 
-prediction = new_model.predict([x_test])
+predictions = new_model.predict([x_test])
 
-print(np.argmax(prediction[0]))
+print(np.argmax(predictions[0])) 
 
-plt.imshow(x_test[0])
+plt.imshow(x_test)
 plt.show()
