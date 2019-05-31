9931a6b2fe6a26a61bdd3d6c3b1e52af016640ea
Anirudh Das Bezzam <abezzam@stevens.edu>
2019-05-30 20:07:40 -0400

    Committs_updated

--- Image_recognizer.py
+++ Image_recognizer.py
@@ -1,7 +1,10 @@
-import numpy as numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import os
+import random
 import cv2
+import pickle
+
 
 DATADIR = ""
 CATEGORIES = ["Dog", "Cat"]
@@ -14,3 +17,44 @@ for category in CATEGORIES:
         plt.show()
         break
     break
+
+#Normalization matrix
+
+IMG_SIZE = 50
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap = 'gray')
+plt.show()
+
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+create_training_data()
+
+random.shuffle(training_data)
+for sample in training_data:
+    x = []
+    y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+pickle_out = open("X.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
