0c69611904ab68b3ebdc29bf4629d0eff52dda23
Anirudh Das Bezzam <abezzam@stevens.edu>
2019-05-30 20:40:10 -0400

    CNN_implemenation

--- Image_recognizer.py
+++ Image_recognizer.py
@@ -4,7 +4,9 @@ import os
 import random
 import cv2
 import pickle
-
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D   
 
 DATADIR = ""
 CATEGORIES = ["Dog", "Cat"]
@@ -57,5 +59,24 @@ pickle_in = open("X.pickle", "rb")
 X = pickle.load(pickle_in)
 pickle_in = open("y.pickle","rb")
 y = pickle.load(pickle_in)
-u
+
+#Implement CNN to categorize
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+X = X/255.0
+
+model=Sequential()
+model.add(Conv2D(64), (3,3), input_shape = X.shape[1:])
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(X,y, batch_size=32, validation_split=0.1)
 
